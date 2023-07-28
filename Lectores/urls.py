6cda3b05b7005d4e1afe4e1615c88de29be05a5c
Israel <isragonnella@gmail.com>
2023-07-27 22:21:12 -0300

    login / logout

--- Lectores/urls.py
+++ Lectores/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from Lectores import views
+from django.contrib.auth.views import LogoutView 
 
 app_name = "Lectores"
 
 urlpatterns = [
-    path('login/', views.login, name='login')
+    path('login/', views.login, name='login'),
+    path('logout/', LogoutView.as_view(template_name='lectores/login.html'), name='logout'),
+    path('registro/', views.registrarse, name='registrarse')
 ]
 
     
--- Lectores/views.py
+++ Lectores/views.py
@@ -1,9 +1,37 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import authenticate,login as django_login
 
 # Create your views here.
-
+ 
 def login (request):
+  
+  if request.method == 'POST':
+    formulario = AuthenticationForm(request, data=request.POST)
+    if formulario.is_valid():
+      usuario = formulario.cleaned_data['username']
+      contrasenia = formulario.cleaned_data['password']
       
-  formulario= AuthenticationForm()  
+      user = authenticate(username=usuario, password=contrasenia)
+      
+      django_login(request, user)
+      return redirect ('inicio:inicio')
+      
+    else:
+        return render (request, 'lectores/login.html', {'formulario': formulario})
+   
+  formulario = AuthenticationForm()     
   return render (request, 'lectores/login.html', {'formulario': formulario})
+
+def registrarse (request):
+  
+  if request.method == 'POST':
+    formulario = AuthenticationForm(request.POST)
+    if formulario.is_valid():
+       formulario.save()
+       return redirect ('lectores:login.html')
+    else:
+        return render (request, 'lectores/registro.html', {'formulario': formulario})
+
+  formulario = UserCreationForm()
+  return render (request, 'lectores/registro.html', {'formulario': formulario})
