74dcb037b3049b9d091ff39fd260108af100bbff
Israel <isragonnella@gmail.com>
2023-08-01 20:41:05 -0300

    login_required

--- inicio/views.py
+++ inicio/views.py
@@ -4,18 +4,21 @@ from inicio.models import Libro, Usuario
 from django.views.generic.edit import UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 def inicio(request):
     return render(request,'inicio/inicio.html')
 
+@login_required
 def crear_libro(request):
     
     if request.method == 'POST':
         formulario = CrearLibroFormulario(request.POST)
         if formulario.is_valid():
             informacion = formulario.cleaned_data
-            libro = Libro(titulo=informacion['titulo'],autor=informacion['autor'],año=informacion['año'],editorial=informacion['editorial'])
+            libro = Libro(titulo=informacion['titulo'],autor=informacion['autor'],año=informacion['año'],editorial=informacion['editorial'], cargado= request.user)
             libro.save()
             return redirect('inicio:lista_libros')
         else:
@@ -61,14 +64,13 @@ class DetalleLibro(DetailView):
     template_name = "inicio/detalle_libros.html"
     
     
-class ModificarLibro(UpdateView):
+class ModificarLibro(UpdateView,LoginRequiredMixin):
     model = Libro
     fields= ['titulo', 'autor', 'año', 'editorial']
     template_name = "inicio/modificar_libros.html"
     success_url = reverse_lazy('inicio:lista_libros')
     
-
-class EliminarLibro(DeleteView):
+class EliminarLibro(DeleteView,LoginRequiredMixin):
     model = Libro
     template_name = "inicio/eliminar_libros.html"
     success_url = reverse_lazy('inicio:lista_libros')
